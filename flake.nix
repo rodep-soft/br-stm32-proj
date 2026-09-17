9fccf70c6d097aa36c49709811fec4f8669e6c30
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-09-17 16:20:36 +0900

    Add Nix Flake and direnv configuration for hermetic dev environment

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "STM32F767ZI Zenoh-Pico & Micro-CDR development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc-arm-embedded
+            cmake
+            ninja
+            gnumake
+            python3
+            stlink
+            openocd
+            git
+          ];
+
+          shellHook = ''
+            echo "=========================================================="
+            echo "  STM32F767ZI Zenoh-Pico Dev Shell (Nix Flake)"
+            echo "=========================================================="
+            echo "Compiler: $(arm-none-eabi-gcc --version | head -n 1)"
+            echo "CMake:    $(cmake --version | head -n 1)"
+            echo "Commands: make build, make flash, make test, make help"
+            echo "=========================================================="
+          '';
+        };
+      });
+    };
+}
